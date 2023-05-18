f4a370f348089f8f9aa643c037a02e632934929c
Joel Jaison <105162603+JoelJaison394@users.noreply.github.com>
2023-05-18 11:29:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.get('/profile/:username', async (req, res) => {
+  const { username } = req.params;
+  try {
+    const url = `https://auth.geeksforgeeks.org/user/${username}`;
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    // Scrape profile information
+    const name = $('.profile_name').text().trim();
+    const institution = $('.basic_details_data a').text().trim();
+    const longestStrek = $('.streakCnt.tooltipped').text().trim();
+    const rank = $('.rankNum').text().trim();
+    const coddingScore = $('.score_card_value').text().trim();
+    const overallScore = coddingScore.slice(0, 3);
+    const solvedProblemsCount = coddingScore.slice(3).replace(/_/g, '');
+    const totalSubmissions = $('.heatmap_header .numProblems').text().trim();
+    // ... add more data points as needed
+
+    // Scrape solved problems
+    const solvedProblems = [];
+    $('.problemdiv .row .col.m6.s12')
+      .find('.problemLink')
+      .each((_, element) => {
+        const question = $(element).text().trim();
+        const link = $(element).attr('href');
+        solvedProblems.push({ question, link });
+      });
+
+    // Construct the profile object
+    const profile = {
+      name,
+      username,
+      institution,
+      rank,
+      overallScore,
+      solvedProblemsCount,
+      longestStrek,
+      totalSubmissions,
+      solvedProblems,
+      // ... add more data points as needed
+    };
+
+    res.json(profile);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Failed to fetch profile data' });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
